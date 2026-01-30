894ae321894fa87492e7654e6c27f4fb34d0fb3f
jakubzawadzkii <chx101558@student.chorzow.merito.pl>
2026-01-30 14:53:21 +0100

    collisions and score

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 class Program
@@ -18,18 +19,40 @@ class Program
 
     static Direction direction = Direction.Right;
 
+    static (int x, int y) food;
+    static int score = 0;
+    static Random rand = new Random();
+
     static void Main()
     {
         Console.CursorVisible = false;
+        food = GenerateFood();
 
         while (true)
         {
             HandleInput();
             MoveSnake();
+
+            if (HitWall() || HitSelf())
+                break;
+
+            if (snake[0] == food)
+            {
+                score++;
+                snake.Add(snake[^1]);
+                food = GenerateFood();
+            }
+
             Draw();
             Thread.Sleep(120);
         }
+
+        Console.Clear();
+        Console.WriteLine("GAME OVER");
+        Console.WriteLine($"Score: {score}");
+        Console.ReadKey();
     }
+
     static void Draw()
     {
         Console.Clear();
@@ -50,13 +73,18 @@ class Program
             Console.Write("X");
         }
 
+        Console.SetCursorPosition(food.x, food.y);
+        Console.Write("O");
+
         foreach (var chunk in snake)
         {
             Console.SetCursorPosition(chunk.x, chunk.y);
             Console.Write("O");
         }
-    }
 
+        Console.SetCursorPosition(0, height);
+        Console.Write($"Score: {score}");
+    }
     static void MoveSnake()
     {
         var head = snake[0];
@@ -73,6 +101,7 @@ class Program
         snake.Insert(0, newHead);
         snake.RemoveAt(snake.Count - 1);
     }
+
     static void HandleInput()
     {
         if (!Console.KeyAvailable)
@@ -82,18 +111,43 @@ class Program
 
         switch (input)
         {
-            case ConsoleKey.UpArrow:
+            case ConsoleKey.UpArrow when direction != Direction.Down:
                 direction = Direction.Up;
                 break;
-            case ConsoleKey.DownArrow:
+            case ConsoleKey.DownArrow when direction != Direction.Up:
                 direction = Direction.Down;
                 break;
-            case ConsoleKey.LeftArrow:
+            case ConsoleKey.LeftArrow when direction != Direction.Right:
                 direction = Direction.Left;
                 break;
-            case ConsoleKey.RightArrow:
+            case ConsoleKey.RightArrow when direction != Direction.Left:
                 direction = Direction.Right;
                 break;
         }
+    }
+
+    static bool HitWall()
+    {
+        var head = snake[0];
+        return head.x <= 0 || head.x >= width - 1 ||
+               head.y <= 0 || head.y >= height - 1;
+    }
+
+    static bool HitSelf()
+    {
+        var head = snake[0];
+        return snake.Skip(1).Contains(head);
+    }
+
+    static (int x, int y) GenerateFood()
+    {
+        (int x, int y) pos;
+        do
+        {
+            pos = (rand.Next(1, width - 2), rand.Next(1, height - 2));
+        }
+        while (snake.Contains(pos));
+
+        return pos;
     }
 }
